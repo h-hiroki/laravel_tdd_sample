56277839b6f62536164b86f63d189de90581dcaf
hhiroki <h-hiroki@users.noreply.github.com>
2019-01-02 17:49:04 +0900

    ⛳️Controllerを使うようにリファクタリング

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function getCustomers()
+    {
+        return response()->json(\App\Customer::query()->select(['id', 'name'])->get());
+    }
+
+    public function postCustomers(Request $request)
+    {
+        // 仮実装
+        if (!$request->json('name')) {
+            return response()->json(
+                [],
+                \Illuminate\Http\Response::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
+        $customer = new \App\Customer();
+        $customer->name = $request->json('name');
+        $customer->save();
+    }
+}
